4db52e3850f7282ae1c046b6d660f59253cdb696
letssharepie <maxleijon@live.se>
2024-05-16 21:58:46 +0200

    base design, take over mate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
-import Image from "next/image";
-
+'use client'
 export default function Home() {
   function inputChanged(input: any) {
-    
+    console.log(input.target.value)
 
+    // .replace(/\s/g, ', ')
+  }
+  function sendToBackend() {
+    console.log('send to backend')
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-6">
-        <label htmlFor="large-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Large input</label>
-        <input onChange={(e) => inputChanged(e.target.value)} type="text" id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-      </input>
-      </div>  
+      <div className="flex mb-6">
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8 " maxLength={2} />
+        <input type="text" onChange={inputChanged} className="border border-gray-50 rounded-md px-2 py-2 mr-2 w-8" maxLength={2} />
+        <button onClick={sendToBackend} className="border border-gray-50 rounded-md px-2 py-2 mr-2 shadow-xl">Send</button>
+      </div>
     </main>
   );
 }
